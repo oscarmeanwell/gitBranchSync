44cf0cfb9333e2cfb245003b3c9159f0845d872a
Oscar Meanwell <oscar.meanwell@gmail.com>
2018-04-09 20:10:26 +0100

    Update mb.sh

--- mb.sh
+++ mb.sh
@@ -10,22 +10,34 @@ read -n 1 answer
 if [ "$answer" = "y" ]; then
     printf "\nLets do it\n"
     git checkout "$b1"
+    printf "\n"
     git pull origin master
+    printf "\n"
     git push
+    printf "\n"
     git checkout master
-    printf "\n Relax, Changes Synced!\n"
+    printf "\n"
+    printf "Relax, Changes Synced!\n"
 else
     printf "\n\n"
     echo -n "Want me to do it for you? y/n: "
     read -n 1 ans
     if [ "$ans" = "y" ]; then
+        printf "\n"
         git add .
+        printf "\n"
         git commit -m "Automated commit"
+        printf "\n"
         git push
+        printf "\n"
         git checkout "$b1"
+        printf "\n"
         git pull origin master
+        printf "\n"
         git push
+        printf "\n"
         git checkout master
+        printf "\n"
         printf "\nRelax, Changes Synced!\n\n"
     else
         printf "\nDont trust me do you?\n"
